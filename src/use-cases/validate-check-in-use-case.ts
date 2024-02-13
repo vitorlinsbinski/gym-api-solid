713a4fad9501576af18970db0aa4db96ab8a5e37
Vitor Linsbinski <vitor.linsbinski@hotmail.com>
2024-02-13 14:26:39 -0400

    add: time validation on check-in use case and unit tests

--- src/use-cases/validate-check-in-use-case.ts
+++ src/use-cases/validate-check-in-use-case.ts
@@ -5,6 +5,8 @@ import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 import { MaxDistanceError } from './errors/max-distance-error';
 import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error';
+import dayjs from 'dayjs';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
@@ -24,6 +26,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validated_at = new Date();
