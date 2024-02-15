39e33b1cb20fa39bca2dbd0acc871d7a1a9e3339
Vitor Linsbinski <vitor.linsbinski@hotmail.com>
2024-02-15 09:41:23 -0400

    add: refresh token controller and e2e test

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from './register-controller';
 import { authenticate } from './authenticate-controller';
 import { profile } from './profile-controller';
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
+import { refresh } from './refresh-controllert';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
 
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
